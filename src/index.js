059a25cc693f9637ded0e2a2be7c8ac6a8e10c1d
Viktor Hesselbom <viktor@hesselbom.net>
2018-03-18 21:59:28 +0100

    Add Collision to export

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Ball from './Ball'
 import Box from './Box'
+import Collision from './Collision'
 import Vector from './Vector'
 import World from './World'
 
 export default {
   Ball,
   Box,
+  Collision,
   Vector,
   World
 }
